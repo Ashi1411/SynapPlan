cb1db88e5c6b7ea919101e4212f4404d7939b528
Ashi Jain <ashi.jain.1411@gmail.com>
2026-03-14 17:13:25 +0530

    Added session collection schema

---
+++ backend/models/session.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const sessionSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    subjectId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Subject",
+        required: true
+    },
+
+    date: {
+        type: Date,
+        required: true,
+    },
+
+    topics: [String],
+
+    duration: {
+        type: Number,
+        required: true
+    },
+
+    durationCompleted: {
+        type: Number,
+        default: 0
+    },
+
+    breakDuration: {
+        type: Number,
+        default: 0
+    },
+
+    breakCount: {
+        type: Number,
+        default: 0
+    },
+
+    completed: {
+        type: Boolean,
+        default: false
+    },
+
+    startTime: Date,
+    endTime: Date,
+
+    status: {
+        type: String,
+        enum: ["pending", "active", "completed", "missed"],
+        default: "pending"
+    }
+    
+}, {timestamps: true});
+
+const Session = mongoose.model("session", sessionSchema);
+
+module.exports = Session;
